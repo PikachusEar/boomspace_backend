da6fbecf66b4463f53cb6d428a908ec22620fa27
PikachusEar <jhexzengzhaoming@163.com>
2024-11-18 16:41:22 -0500

    Admin Title Change

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from .views import home, wechat_login, available_timeslots, make_reservation, view_reservations_and_balance, view_user_info, update_user_info, get_images, make_combo_reservation
+from django.contrib import admin
+
+# 修改admin站点的标题
+admin.site.site_header = 'Boomsapce Administration'    # 设置网站页头
+admin.site.site_title = 'Boomspace Admin'         # 设置网站标题
+admin.site.index_title = 'Boomspace Admin'        # 设置首页标题
+
 urlpatterns = [
     path('', home, name='home'),
     path('wechat_login', wechat_login, name='wechat_login'),
